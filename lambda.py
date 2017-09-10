90c2b40e1af432f069e78862007cf8c26e820764
Aaron Gorka <aaron.gorka@amaysim.com.au>
2017-09-10 19:47:20 +1000

    Add button functionality, fix correlation-id logging, split out functions
    Remove broken test data, will add proper cleansed data in later

--- lambda.py
+++ lambda.py
@@ -9,93 +9,130 @@ import mysql.connector
 from urllib.parse import urlparse, parse_qs
 
 
+def button_handler(event, context):
+    """Handler for button events"""
+
+    loglevel = os.environ.get('LOGLEVEL', 'INFO')
+    aws_lambda_logging.setup(level=loglevel, env=os.environ.get('ENV'))
+    logging.debug(json.dumps({'action': 'initialising'}))
+    logging.debug(json.dumps({'event': event}))
+
+    try:
+        body = parse_qs(event['body'])
+        payload = json.loads(body['payload'][0])
+    except Exception as e:
+        logging.exception(json.dumps({'action': 'parse payload', 'status': 'failed', 'error': str(e)}))
+    else:
+        logging.debug(json.dumps({'action': 'parse payload', 'status': 'success', 'payload': payload}))
+
+    correlation_id = payload['trigger_id']  # Use correlation ID from Slack
+    logging.debug(json.dumps({'correlation_id': correlation_id}))
+    aws_lambda_logging.setup(level=loglevel, env=os.environ.get('ENV'), correlation_id=correlation_id)
+
+    selected_alias = payload['actions'][0]['value']
+    logging.debug(json.dumps({'selected_alias': selected_alias}))
+
+    response = lookup_alias_and_execute(selected_alias)
+
+    logging.info(json.dumps({'action': 'responding', 'response': response}))
+    return response
+
+
 def handler(event, context):
     """Main entrypoint"""
 
     loglevel = os.environ.get('LOGLEVEL', 'INFO')
-    logging.debug('.(setup) #1')
-    aws_lambda_logging.setup(level=loglevel)
-    logging.debug('.(setup) #2')
-    aws_lambda_logging.setup(level=loglevel)  # for some reason you have to do setup twice
-    try:
-        env = os.environ.get('ENV')
-        aws_lambda_logging.setup(env=env)
-    except:
-        pass
-    logging.debug('Dumping event #1...')
+    aws_lambda_logging.setup(level=loglevel, env=os.environ.get('ENV'))
+    logging.debug(json.dumps({'action': 'initialising'}))
     logging.debug(json.dumps({'event': event}))
-    logging.debug('Dumping event #2...')
-
-    body = parse_qs(event['body'])
-
-    logging.debug(json.dumps({'body': body}))
 
     try:
-        correlation_id = body['trigger_id'][0]  # Use correlation ID from Slack
-        logging.debug(json.dumps({'correlation_id': correlation_id}))
-        aws_lambda_logging.setup(correlation_id=correlation_id)
-    except:
-        pass
+        body = parse_qs(event['body'])
+    except Exception as e:
+        logging.exception(json.dumps({'action': 'parse body', 'status': 'failed', 'error': str(e)}))
+    else:
+        logging.debug(json.dumps({'action': 'parse body', 'status': 'success', 'body': body}))
 
+    correlation_id = body['trigger_id'][0]  # Use correlation ID from Slack
+    logging.debug(json.dumps({'correlation_id': correlation_id}))
+    aws_lambda_logging.setup(level=loglevel, env=os.environ.get('ENV'), correlation_id=correlation_id)
+
+    message_type = categorise_message(body)
+    logging.debug(json.dumps({'message type': message_type}))
+
+    if body.get('text'):
+        try:
+            selected_alias = body['text'][0]
+        except KeyError as e:
+            logging.exception(json.dumps({'action': 'get selected_alias', 'status': 'failed', 'error': str(e)}))
+        else:
+            logging.debug(json.dumps({'action': 'get selected_alias', 'status': 'success'}))
+    elif body.get('actions'):
+        selected_alias = body['actions'][0]['value']
+
+    response = lookup_alias_and_execute(selected_alias)
+
+    logging.info(json.dumps({'action': 'responding', 'response': response}))
+    return response
+
+
+def get_config():
     with open("example.yml") as stream:
         try:
             config = yaml.safe_load(stream)
             logging.debug(json.dumps({'config': config}))
         except yaml.YAMLError as e:
             logging.exception(json.dumps({'action': 'load yaml', 'status': 'failed', 'error': str(e)}))
+        else:
+            logging.debug(json.dumps({'action': 'load yaml', 'status': 'success'}))
+    return config
 
-    user_selection = body['text'][0]
 
-    query = retrieve_query(config, user_selection)
+def lookup_alias_and_execute(selected_alias):
+    """Executes a user's selected alias by looking up details from the config file, returns a message that can be returned to Slack"""
 
-    if query:
-        try:
-            result = run_query(query)
-        except Exception as e:
-            logging.exception(json.dumps({'action': 'running SQL query', 'status': 'failed', 'error': str(e)}))
-    
-        body = ""
-        try:
-            body = format_result(result)
-        except Exception as e:
-            logging.exception(json.dumps({'action': 'formatting response', 'status': 'failed', 'error': str(e)}))
+    config = get_config()
+    query = get_query_details(config, selected_alias)
+
+    if query == "missing_alias":
+        response = missing_alias_message(config['queries'], selected_alias)
     else:
-        attachments = format_attachments(config['queries'])
-        body = {
-            "response_type": "in_channel",
-            "text": "The alias `{}` doesn't exist. Here are the available aliases you may call:".format(user_selection),
-            "attachments": attachments
-        }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body),
-        'headers': {
-            'Content-Type': 'application/json',
-        }
-    }
-        
-    logging.info(json.dumps({'action': 'responding', 'response': response}))
+        result = run_query(query)
+        response = format_query_result(result)
     return response
 
-def retrieve_query(config, user_selection):
+
+def categorise_message(body):
+    command = body.get('command', None)
+    actions = body.get('actions', None)
+    if command is not None and "/sql" in command:
+        input_type = 'slashcommand'
+    elif actions is not None:
+        input_type = 'executebutton'
+    else:
+        input_type = 'other'
+    return input_type
+
+
+def get_query_details(config, selected_alias):
     """Checks that the user's selection exists in the configuration file and returns the data associated with it"""
 
-    for alias in config['queries']:
-        if alias == user_selection:
-            query = [ x for x in config if config['queries'] == alias ]
+    for query in config['queries']:
+        if query['alias'] == selected_alias:
             return query
     else:
         logging.debug(json.dumps('The selection the user has made does not exist in the configuration file'))
-        return None
+        return "missing_alias"
 
-def format_attachments(queries):
-    """Formats all available queries as a Slack attachment, returns an attachments object"""
+
+def missing_alias_message(queries, selected_alias):
+    """Returns a mesage about the alias missing and provides valid aliases to execute"""
 
     attachments = []
     for query in queries:
         attachments.append({
             "color": "#36a64f",
+            "callback_id": "comic_1234_xyz",
             "fields": [
                 {
                     "title": "Alias",
@@ -116,43 +153,70 @@ def format_attachments(queries):
                     "title": "Database",
                     "value": query['mysql_database'],
                     "short": True
-                },
+                }
+            ],
+            "actions": [
                 {
-                    "title": "Button",
-                    "value": "TODO",  # TODO
-                    "short": True
+                    "name": "execute",
+                    "text": "execute",
+                    "type": "button",
+                    "value": query['alias']
                 }
             ],
             "fallback": "alias: {}, statement: {}".format(query['alias'], query['sql'])
         })
     logging.debug(json.dumps({'attachments': attachments}))
-    return attachments
+
+    body = {
+        "response_type": "in_channel",
+        "text": "The alias `{}` doesn't exist. Here are the available aliases you may call:".format(selected_alias),
+        "attachments": attachments
+    }
+    response = format_response(body)
+    return response
 
 
 def run_query(query):
     """Takes a query from the configuration file, executes it and returns the result"""
 
-    try:
-        cnx = mysql.connector.connect(
-        user=os.environ['MYSQL_USER'], 
-        password=os.environ['MYSQL_PASSWORD'], 
-        database=query['mysql_host'], 
-        host=query['mysql_host'])
-    except:
-        logging.exception("Failed to connect to MySQL database")
-    cur = cnx.cursor(buffered=True)
-    cur.execute(query['sql'])
-    result = cur.fetchall()
-    return result
+    #try:
+    #    cnx = mysql.connector.connect(
+    #        user=os.environ['MYSQL_USER'],
+    #        password=os.environ['MYSQL_PASSWORD'],
+    #        database=query['mysql_host'],
+    #        host=query['mysql_host']
+    #    )
+    #except:
+    #    logging.exception("Failed to connect to MySQL database")
+    #cur = cnx.cursor(buffered=True)
+    #cur.execute(query['sql'])
+    #result = cur.fetchall()
+    return "some sql result asdfasdfasdf\nasdfasdfasdf\nasdfasdfasdf\nasdffdsaasdfasdfasdf"
 
 
-def format_result(result):
+def format_response(body):
+    """Formats the Slack response to work with lambda-proxy"""
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+    }
+    return response
+
+
+def format_query_result(result):
     """Formats the query results in to a format accepted by Slack"""
+
     body = "```{}```".format(result)  # Add results as preformatted string
-    return body
+    response = format_response(body)
+    return response
 
 
 def main():
-    handler(json.loads('{"resource": "/hello", "path": "/hello", "httpMethod": "POST", "headers": {"Accept": "application/json,*/*", "Accept-Encoding": "gzip,deflate", "CloudFront-Forwarded-Proto": "https", "CloudFront-Is-Desktop-Viewer": "true", "CloudFront-Is-Mobile-Viewer": "false", "CloudFront-Is-SmartTV-Viewer": "false", "CloudFront-Is-Tablet-Viewer": "false", "CloudFront-Viewer-Country": "US", "Content-Type": "application/x-www-form-urlencoded", "Host": "foobar.execute-api.ap-southeast-2.amazonaws.com", "User-Agent": "Slackbot 1.0 (+https://api.slack.com/robots)", "Via": "1.1 foobar.cloudfront.net (CloudFront)", "X-Amz-Cf-Id": "foobar", "X-Amzn-Trace-Id": "Root=footraceidbar", "X-Forwarded-For": "0.123.456.789, 0.123.456.789", "X-Forwarded-Port": "443", "X-Forwarded-Proto": "https"}, "queryStringParameters": null, "pathParameters": null, "stageVariables": null, "requestContext": {"path": "/Devaaron/hello", "accountId": "01234556678", "resourceId": "2345sd", "stage": "Devaaron", "requestId": "uuid", "identity": {"cognitoIdentityPoolId": null, "accountId": null, "cognitoIdentityId": null, "caller": null, "apiKey": "", "sourceIp": "0.123.345.123", "accessKey": null, "cognitoAuthenticationType": null, "cognitoAuthenticationProvider": null, "userArn": null, "userAgent": "Slackbot 1.0 (+https://api.slack.com/robots)", "user": null}, "resourcePath": "/hello", "httpMethod": "POST", "apiId": "asdfasd"}, "body": "token=3452345asdf&team_id=SDFSDFSDF&team_domain=asdfaq345&channel_id=345asdf&channel_name=general&user_id=2345asdfas&user_name=2345asdf&command=%2Fsql&text=asdf&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%asdfasdf&trigger_id=asdfqw45qwefasdf", "isBase64Encoded": false}'), {})
+    pass
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
